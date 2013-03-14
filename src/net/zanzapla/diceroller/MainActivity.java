6f97729d36a41672d3faae22b29501d18b55ba03
Zanza00 <zanza00@gmail.com>
2013-03-14 17:49:07 +0100

    Chance the roll onClickRoll method tu use string.xml

--- src/net/zanzapla/diceroller/MainActivity.java
+++ src/net/zanzapla/diceroller/MainActivity.java
@@ -1,29 +1,26 @@
 package net.zanzapla.diceroller;
 
-
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
-
 import net.zanzapla.diceroller.core.*;
 
 public class MainActivity extends Activity {
 	
 public final static String EXTRA_MESSAGE = "net.zanzapla.diceroller.MESSAGE";	
-public static Dice dice;
-private TextView resultDisplay, resultDisplayDice;
+public static Dice dice = new Dice();
+private TextView resultText, resultValue;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		resultDisplay = (TextView) findViewById(R.id.resultTextView);
-		resultDisplayDice = (TextView) findViewById(R.id.numberResultTextView);
+		resultText = (TextView) findViewById(R.id.resultTextView);
+		resultValue = (TextView) findViewById(R.id.numberResultTextView);
 	}
 
 	@Override
@@ -33,42 +30,26 @@ private TextView resultDisplay, resultDisplayDice;
 		return true;
 	}
 	public void onClickRoll(View btn){
-		dice = new Dice();
-		dice.setDieFaces();
+		int Qid = Integer.parseInt((String) btn.getTag());		
+		dice.setDieFaces(Qid);
 		dice.roll();
 		String result = dice.getStrValue();
-		String text = "hai tirato un dado da 6";
-		resultDisplayDice.setText(text);
-		resultDisplay.setText(result);
-		
-	}
-	public void onClickRoll100(View btn){
-		dice = new Dice();
-		dice.setDieFaces(100);
-		dice.roll();
-		String result = dice.getStrValue();
-		String text = "hai tirato un dado da 100";
-		resultDisplayDice.setText(text);
-		resultDisplay.setText(result);
-	}
-	public void onClickRoll20(View btn){
-		dice = new Dice();
-		dice.setDieFaces(20);
-		dice.roll();
-		String result = dice.getStrValue();
-		String text = "hai tirato un dado da 20";
-		resultDisplayDice.setText(text);
-		resultDisplay.setText(result);
-		
-	}
-	public void onClickRoll10(View btn){
-		dice = new Dice();
-		dice.setDieFaces(10);
-		dice.roll();
-		String result = dice.getStrValue();
-		String text = "hai tirato un dado da 10";
-		resultDisplayDice.setText(text);
-		resultDisplay.setText(result);
-		
+		String text;
+		switch (Qid) {
+		case 6: text = getResources().getString(R.string.dice_six);
+		break;
+		case 10: text = getResources().getString(R.string.dice_ten);
+		break;
+		case 20: text = getResources().getString(R.string.dice_twenty);
+		break;
+		case 100: text = getResources().getString(R.string.dice_hundred);
+		break;
+
+		default: text = "HALP!";
+		break;
+
+		}
+		resultText.setText(result);
+		resultValue.setText(text);		
 	}
 }
